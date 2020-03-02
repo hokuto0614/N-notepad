bbe89517fc68caa2a7d37011a5b7a7d5652bde65
hokuto <h_miyata@nexus-nt.co.jp>
2020-03-02 22:31:32 +0900

    DB処理一部追加

--- app/src/main/java/com/example/n_notepad/EditActivity.java
+++ app/src/main/java/com/example/n_notepad/EditActivity.java
@@ -1,18 +1,67 @@
 package com.example.n_notepad;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.UUID;
+
 public class EditActivity  extends AppCompatActivity {
+
+    // DataBaseHelperクラスを定義
+    DataBaseHelper helper = null;
+    // 新規フラグ
+    boolean newFlag = false;
+    // id
+    String id = "";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit);
+        // データベースから値を取得する
+        if(helper == null){
+            helper = new DataBaseHelper(EditActivity.this);
+        }
+        // ListActivityからインテントを取得
+        Intent intent = this.getIntent();
+        // 値を取得
+        id = intent.getStringExtra("id");
+        // 画面に表示
+        if(id.equals("")){
+            // 新規作成の場合
+            newFlag = true;
+        }else {
+            // 編集の場合 データベースから値を取得して表示
+            // データベースを取得する
+            SQLiteDatabase db = helper.getWritableDatabase();
+            try {
+                // rawQueryというSELECT専用メソッドを使用してデータを取得する
+                Cursor c = db.rawQuery("select body from MEMO_TABLE where uuid = '" + id + "'", null);
+                // Cursorの先頭行があるかどうか確認
+                boolean next = c.moveToFirst();
+                // 取得した全ての行を取得
+                while (next) {
+                    // 取得したカラムの順番(0から始まる)と型を指定してデータを取得する
+                    String dispBody = c.getString(0);
+                    EditText body = (EditText) findViewById(R.id.editMemoContent);
+                    body.setText(dispBody, TextView.BufferType.NORMAL);
+                    next = c.moveToNext();
+                }
+            } finally {
+                // finallyは、tryの中で例外が発生した時でも必ず実行される
+                // dbを開いたら確実にclose
+                db.close();
+            }
+        }
 
         Button cancelButton = findViewById(R.id.cancelMemoButton);
         Button saveButton = findViewById(R.id.saveMemoButton);
@@ -30,13 +79,34 @@ public class EditActivity  extends AppCompatActivity {
         saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(EditActivity.this, ShowActivity.class);
+                // 入力内容を取得する
+                EditText title = (EditText) findViewById(R.id.editMemoTitle);
+                EditText body = (EditText) findViewById(R.id.editMemoContent);
+                String titleStr = title.getText().toString();
+                String bodyStr = body.getText().toString();
+
+                // データベースに保存する
+                SQLiteDatabase db = helper.getWritableDatabase();
+                try {
+                    if (newFlag) {
+                        // 新規作成の場合
+                        // 新しくidを発行する
+                        id = UUID.randomUUID().toString();
+                        // INSERT
+                        db.execSQL("insert into MEMO_TABLE(uuid, title, body) VALUES('" + id + "', '" + bodyStr + "', '" + titleStr + "')");
+                    } else {
+                        // UPDATE
+                        db.execSQL("update MEMO_TABLE set title = '" + titleStr + "', body = '" + bodyStr + "' where uuid = '" + id + "'");
+                    }
+                } finally {
+                    // finallyは、tryの中で例外が発生した時でも必ず実行される
+                    // dbを開いたら確実にclose
+                    db.close();
+                }
+                // 保存後に一覧へ戻る
+                Intent intent = new Intent(EditActivity.this, MainActivity.class);
                 startActivity(intent);
             }
-
-            //ToDo 保存時DB処理
-
         });
     }
-
 }
--- app/src/main/java/com/example/n_notepad/MainActivity.java
+++ app/src/main/java/com/example/n_notepad/MainActivity.java
@@ -3,19 +3,60 @@ package com.example.n_notepad;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
+
 public class MainActivity extends AppCompatActivity {
+
+    // DataBaseHelperクラスを定義
+    DataBaseHelper helper = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //ToDo DBを参照しリストを作成
+        //DBを参照しリストを作成
+        if(helper == null){
+            helper = new DataBaseHelper(MainActivity.this);
+        }
+        // メモリストデータを格納する変数
+        ArrayList<HashMap<String, String>> memoList = new ArrayList<>();
+        // データベースを取得する
+        SQLiteDatabase db = helper.getWritableDatabase();
+        try {
+            //データを取得する
+            Cursor c = db.rawQuery("select uuid, title, body from MEMO_TABLE order by id", null);
+            // Cursorの先頭行があるかどうか確認
+            boolean next = c.moveToFirst();
 
+            // 取得した全ての行を取得
+            while (next) {
+                HashMap<String,String> data = new HashMap<>();
+                // 取得したカラムの順番(0から始まる)と型を指定してデータを取得する
+                String title = c.getString(1);
+                String body = c.getString(2);
+                if(title.length() > 10){
+                    // リストに表示するのは10文字まで
+                    title = title.substring(0, 11) + "...";
+                }
+                data.put("body",body);
+                data.put("title",title);
+                memoList.add(data);
+                // 次の行が存在するか確認
+                next = c.moveToNext();
+            }
+        } finally {
+            // dbを開いたら確実にclose
+            db.close();
+        }
         //新規作成ボタン押下時処理
         Button addButton = findViewById(R.id.addButton);
         addButton.setOnClickListener(new View.OnClickListener() {
@@ -26,6 +67,5 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        //リストアイテム押下時処理
     }
 }
